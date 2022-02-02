d8164ba360b37fb7e8bbd1ab3afb9934572f8392
Osvaldo Laini <osvaldolaini@hotmail.com>
2022-02-02 02:41:19 -0300

    master

--- resources/views/site/partials/adsNews.blade.php
+++ resources/views/site/partials/adsNews.blade.php
@@ -2,8 +2,8 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article"
-                data-ad-format="fluid" data-ad-client="ca-pub-9377063296356941" data-ad-slot="4503715624"></ins>
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9377063296356941"
+                data-ad-slot="9804045709" data-ad-format="auto" data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
