267b63d971cde96f23f73049b719ef31c9abaaa6
Osvaldo Laini <osvaldolaini@hotmail.com>
2022-02-01 23:00:02 -0300

    master

--- resources/views/site/sections/modalities.blade.php
+++ resources/views/site/sections/modalities.blade.php
@@ -1,5 +1,5 @@
 <!-- ======= Modalities Section ======= -->
-<section class="custom-section section-modalities jarallax" >
+<section class="custom-section section-modalities jarallax">
     <div class="container">
         <div class="row">
             <div class="col">
@@ -10,70 +10,76 @@
         </div>
         <div class="row">
             <div class="col-lg-3 mb-3 card-modalities" data-aos="fade-up">
-                <a class="modalities-item" href="{{url('modalidades#crossfit')}}" data-toggle="tooltip"
-                    title="CrossFit"> 
-                    <div class="title">   
+                <a class="modalities-item" href="{{ url('modalidades#crossfit') }}" data-toggle="tooltip"
+                    title="CrossFit">
+                    <div class="title">
                         CrossFit
                     </div>
                     <div class="image">
                         <picture>
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit.jpg')}}" class="lazyload ">
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit.webp')}}" class="lazyload ">
-                            <img class="img-fluid" data-src="{{url('storage/images/site/canoas-crossfit.webp')}}"
-                                alt="{{$config->meta_tags}}-crossfit" class="lazyload ">
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit.jpg') }}"
+                                class="lazyload ">
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit.webp') }}"
+                                class="lazyload ">
+                            <img class="img-fluid" data-src="{{ url('storage/images/site/canoas-crossfit.webp') }}"
+                                alt="{!! $config->meta_tags !!}-crossfit" class="lazyload ">
                         </picture>
                     </div>
                 </a>
             </div>
             <div class="col-lg-3 mb-3 card-modalities" data-aos="fade-up" data-aos-delay="200">
-                <a class="modalities-item" href="{{url('modalidades#lpo')}}" data-toggle="tooltip" title="Levantamento de peso">
+                <a class="modalities-item" href="{{ url('modalidades#lpo') }}" data-toggle="tooltip"
+                    title="Levantamento de peso">
                     <div class="title">
                         Levantamento de peso
                     </div>
                     <div class="image">
                         <picture>
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-lpo.jpg')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-lpo.jpg') }}"
                                 class="lazyload ">
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-lpo.webp')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-lpo.webp') }}"
                                 class="lazyload ">
-                            <img class="img-fluid" data-src="{{url('storage/images/site/canoas-crossfit-lpo.webp')}}"
-                                alt="{{$config->meta_tags}}-levantamento-de-peso" class="lazyload ">
+                            <img class="img-fluid"
+                                data-src="{{ url('storage/images/site/canoas-crossfit-lpo.webp') }}"
+                                alt="{!! $config->meta_tags !!}-levantamento-de-peso" class="lazyload ">
                         </picture>
                     </div>
                 </a>
             </div>
             <div class="col-lg-3 mb-3 card-modalities" data-aos="fade-up" data-aos-delay="400">
-                <a class="modalities-item" href="{{url('modalidades#gym')}}" data-toggle="tooltip" title="Movimentos Ginásticos">    
+                <a class="modalities-item" href="{{ url('modalidades#gym') }}" data-toggle="tooltip"
+                    title="Movimentos Ginásticos">
                     <div class="title">
                         Movimentos Ginásticos
                     </div>
                     <div class="image">
                         <picture>
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-ginastica.jpg')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-ginastica.jpg') }}"
                                 class="lazyload ">
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-ginastica.webp')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-ginastica.webp') }}"
                                 class="lazyload ">
                             <img class="img-fluid"
-                                data-src="{{url('storage/images/site/canoas-crossfit-ginastica.webp')}}" alt="{{$config->meta_tags}}-ginastica"
-                                class="lazyload ">
+                                data-src="{{ url('storage/images/site/canoas-crossfit-ginastica.webp') }}"
+                                alt="{!! $config->meta_tags !!}-ginastica" class="lazyload ">
                         </picture>
                     </div>
                 </a>
             </div>
             <div class="col-lg-3 mb-3 card-modalities" data-aos="fade-up" data-aos-delay="600">
-                <a class="modalities-item" href="{{url('modalidades#mobilidade')}}" data-toggle="tooltip" title="Mobilidades">
+                <a class="modalities-item" href="{{ url('modalidades#mobilidade') }}" data-toggle="tooltip"
+                    title="Mobilidades">
                     <div class="title">
                         Mobilidades
                     </div>
                     <div class="image">
                         <picture>
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-mobilidade.jpg')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-mobilidade.jpg') }}"
                                 class="lazyload ">
-                            <source data-srcset="{{url('storage/images/site/canoas-crossfit-mobilidade.webp')}}"
+                            <source data-srcset="{{ url('storage/images/site/canoas-crossfit-mobilidade.webp') }}"
                                 class="lazyload ">
                             <img class="img-fluid"
-                                data-src="{{url('storage/images/site/canoas-crossfit-mobilidade.webp')}}" alt="{{$config->meta_tags}}-mobilidade"
-                                class="lazyload ">
+                                data-src="{{ url('storage/images/site/canoas-crossfit-mobilidade.webp') }}"
+                                alt="{!! $config->meta_tags !!}-mobilidade" class="lazyload ">
                         </picture>
                     </div>
                 </a>
